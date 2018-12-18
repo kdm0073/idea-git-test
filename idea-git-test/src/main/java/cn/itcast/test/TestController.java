6133fcfb8ee416f240e90a55c67c4ec174313b6b
kdm <kongdeming@itcast.cn>
2018-12-18 18:03:56 +0800

    idea提交工程到本地git库

---
+++ idea-git-test/src/main/java/cn/itcast/test/TestController.java
@@ -0,0 +1,17 @@
+package cn.itcast.test;
+
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+    }
+}
