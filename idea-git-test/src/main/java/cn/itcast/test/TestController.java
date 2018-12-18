4c570bd373f19f4707147629efc7ee3857cc0bfb
kdm <kongdeming@itcast.cn>
2018-12-18 19:54:40 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	idea-git-test/src/main/java/cn/itcast/test/TestController.java

--- idea-git-test/src/main/java/cn/itcast/test/TestController.java
+++ idea-git-test/src/main/java/cn/itcast/test/TestController.java
@@ -30,8 +30,13 @@ public class TestController {
         System.out.println("aaa");
 
     }
-    public void a(){
+    public void a() {
         System.out.println("a");
-
+    }
+    public void b(){
+        System.out.println("b");
+    }
+    public void c(){
+        System.out.println("c");
     }
 }
